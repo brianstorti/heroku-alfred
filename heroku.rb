6f7d15e3ba50f0e3ab219d91f9cdca9a627c8b3a
Brian Thomas Storti <btstorti@gmail.com>
2018-07-07 14:46:28 -0300

    Create alfred workflow to search heroku apps

---
+++ heroku.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+
+require 'json'
+
+CACHE_FILE = '.appscache'
+
+def update_apps_list
+  app_names = `/usr/local/bin/heroku apps --all | grep -v '===' | awk '{print $1}' | awk NF`
+  File.open(CACHE_FILE, 'w') { |f| f.write(app_names) }
+  apps_list_cached
+end
+
+def apps_list_cached
+  File.read(CACHE_FILE).split("\n") if File.exist?(CACHE_FILE)
+end
+
+def apps_list
+  apps = apps_list_cached
+  return apps if apps && !apps.empty?
+
+  update_apps_list
+end
+
+def filtered_apps_list(query)
+  return apps_list if query.empty?
+  apps_list.select { |app| app =~ Regexp.new(query, 'i') }
+end
+
+if ARGV.first == "--refresh"
+  update_apps_list
+else
+  query = ARGV.first || ''
+  apps = filtered_apps_list(query).map do |app|
+    {
+      "uid": app,
+      "title": app,
+      "subtitle": "open app in your browser",
+      "arg": app,
+      "valid": true
+    }
+  end
+
+  print({ items: apps }.to_json)
+end
